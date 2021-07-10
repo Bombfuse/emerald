cb9bdeed05cd2f57b0a64982b7206fe59dbb28f7
Ilia Konnov <ilia@sldr.xyz>
2021-07-10 20:32:55 +0300

    Mouse input

--- src/input/button_state.rs
+++ src/input/button_state.rs
@@ -1,18 +1,20 @@
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Default)]
 pub struct ButtonState {
     pub is_pressed: bool,
     pub was_pressed: bool,
 }
 impl ButtonState {
     pub fn new() -> Self {
-        ButtonState {
-            is_pressed: false,
-            was_pressed: false,
-        }
+        ButtonState::default()
     }
 
     #[inline]
     pub(crate) fn rollover(&mut self) {
         self.was_pressed = self.is_pressed;
+    }
+
+    #[inline]
+    pub fn is_just_pressed(&self) -> bool {
+        !self.was_pressed && self.is_pressed
     }
 }
