b31c43fc4584c488c200fefa5f0fa823c1a9e8c5
Bombfuse <eiffeldud@gmail.com>
2021-08-02 20:39:19 -0700

    Expose snd instance set_volume in mixer handler

--- src/audio/mixer.rs
+++ src/audio/mixer.rs
@@ -46,6 +46,7 @@ impl<'a> MixerHandler<'a> {
     pub fn play(&mut self, key: SoundKey) -> Result<SoundInstanceId, EmeraldError> { self.inner.play(key, &mut self.asset_store) }
     pub fn play_and_loop(&mut self, key: SoundKey) -> Result<SoundInstanceId, EmeraldError> { self.inner.play_and_loop(key, &mut self.asset_store) }
     pub fn get_volume(&self) -> Result<f32, EmeraldError> { self.inner.get_volume() }
+    pub fn set_instance_volume(&mut self, snd_instance_id: SoundInstanceId, volume: f32) -> Result<(), EmeraldError> { self.inner.set_instance_volume(snd_instance_id, volume) }
     pub fn set_volume(&mut self, volume: f32) -> Result<(), EmeraldError> { self.inner.set_volume(volume) }
     pub fn get_instances(&self) -> Result<Vec<SoundInstanceId>, EmeraldError> { self.inner.get_instances() }
     pub fn stop(&mut self, snd_instance_id: SoundInstanceId) -> Result<(), EmeraldError> { self.inner.stop(snd_instance_id) }
