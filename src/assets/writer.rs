8879dffa455d01e1280d09411e26b78277b8e80c
bombfuse <eiffeldud@gmail.com>
2021-08-22 15:57:53 -0700

    Add user data directory for Windows. Saving to user files. (#86)

--- src/assets/writer.rs
+++ src/assets/writer.rs
@@ -2,15 +2,20 @@ use crate::EmeraldError;
 use std::fs::File;
 use std::io::prelude::Write;
 
-#[derive(Copy, Clone)]
-pub struct Writer {}
+#[derive(Clone)]
+pub struct Writer {
+    user_directory: String,
+}
 impl Writer {
-    pub(crate) fn new() -> Self {
-        Writer {}
+    pub(crate) fn new(user_directory: String) -> Self {
+        Writer {
+            user_directory,
+        }
     }
 
-    pub fn write<T: Into<String>>(&mut self, bytes: &[u8], path: T) -> Result<(), EmeraldError> {
-        let mut file = File::create(path.into())?;
+    pub fn write_to_user_file<T: Into<String>>(&mut self, bytes: &[u8], relative_path: T) -> Result<(), EmeraldError> {
+        let path = self.user_directory.clone() + &relative_path.into();
+        let mut file = File::create(path)?;
         file.write_all(bytes)?;
 
         Ok(())
