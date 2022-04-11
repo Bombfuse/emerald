7e3077b0bd8002db674eacf954c458aaa5b8ad29
sapir <yasapir@gmail.com>
2022-04-12 01:06:48 +0300

    Rectangle helpers (#152)
    * Add various helper methods to Rectangle
    
    * Fix off-by-one in Rectangle::intersects_with()

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,4 @@
+use glam::{vec2, Vec2};
 use nanoserde::DeJson;
 
 #[derive(Clone, Copy, Debug, DeJson)]
@@ -9,11 +10,24 @@ pub struct Rectangle {
 }
 impl Rectangle {
     pub fn new(x: f32, y: f32, width: f32, height: f32) -> Self {
-        Rectangle {
+        Self {
             x,
             y,
             width,
             height,
+        }
+    }
+
+    /// Creates a `Rectangle` from its bottom-left point and its size.
+    pub fn from_point_and_size(point: impl Into<Vec2>, size: impl Into<Vec2>) -> Self {
+        let point = point.into();
+        let size = size.into();
+
+        Self {
+            x: point.x,
+            y: point.y,
+            width: size.x,
+            height: size.y,
         }
     }
 
@@ -26,11 +40,48 @@ impl Rectangle {
         self.width == 0.0 && self.height == 0.0
     }
 
+    /// An alias for self.x
+    #[inline]
+    pub fn left(&self) -> f32 {
+        self.x
+    }
+
+    #[inline]
+    pub fn right(&self) -> f32 {
+        self.x + self.width
+    }
+
+    /// An alias for self.y
+    #[inline]
+    pub fn bottom(&self) -> f32 {
+        self.y
+    }
+
+    #[inline]
+    pub fn top(&self) -> f32 {
+        self.y + self.height
+    }
+
+    #[inline]
+    pub fn bottom_left(&self) -> Vec2 {
+        vec2(self.left(), self.bottom())
+    }
+
+    #[inline]
+    pub fn size(&self) -> Vec2 {
+        vec2(self.width, self.height)
+    }
+
+    #[inline]
+    pub fn center(&self) -> Vec2 {
+        self.bottom_left() + self.size() / 2.0
+    }
+
     /// Whether or not the given rectangle and this rectangle intersect
     pub fn intersects_with(&self, other: &Rectangle) -> bool {
-        self.x <= (other.x + other.width)
-            && (self.x + self.width) >= other.x
-            && self.y <= other.y + other.height
-            && self.y + self.height >= other.y
+        self.x < other.x + other.width
+            && self.x + self.width > other.x
+            && self.y < other.y + other.height
+            && self.y + self.height > other.y
     }
 }
