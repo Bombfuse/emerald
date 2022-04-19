6b3a9d660d1487cb518c570f201e4e50673eaea6
sapir <yasapir@gmail.com>
2022-04-19 13:07:09 +0300

    Avoid cloning AsepriteTag

--- src/rendering/components/aseprite.rs
+++ src/rendering/components/aseprite.rs
@@ -10,7 +10,7 @@ use types::*;
 #[derive(Clone, Debug)]
 pub struct Aseprite {
     pub(crate) data: Arc<AsepriteData>,
-    pub(crate) current_tag: AsepriteTag,
+    pub(crate) current_tag_index: Option<usize>,
     pub(crate) sprite: Sprite,
     pub(crate) elapsed_time: f32,
     pub(crate) total_anim_elapsed_time: f32,
@@ -48,7 +48,7 @@ impl Aseprite {
             elapsed_time: 0.0,
             total_anim_elapsed_time: 0.0,
             frame_counter: 0,
-            current_tag: AsepriteTag::default(),
+            current_tag_index: None,
             sprite,
             is_looping: false,
             rotation: 0.0,
@@ -63,8 +63,15 @@ impl Aseprite {
         Ok(aseprite)
     }
 
+    fn get_current_tag(&self) -> Option<&AsepriteTag> {
+        self.current_tag_index
+            .map(|idx| &self.data.meta.frame_tags[idx])
+    }
+
     pub fn get_animation_name(&self) -> &str {
-        &self.current_tag.name
+        self.get_current_tag()
+            .map(|tag| tag.name.as_str())
+            .unwrap_or("")
     }
 
     pub fn get_elapsed_time(&self) -> f32 {
@@ -107,8 +114,8 @@ impl Aseprite {
     fn set_tag<T: AsRef<str>>(&mut self, animation_name: T) -> Result<(), EmeraldError> {
         let animation_name: &str = animation_name.as_ref();
 
-        if let Some(tag) = self.get_tag(animation_name) {
-            self.current_tag = tag;
+        if let Some(idx) = self.find_tag(animation_name) {
+            self.current_tag_index = Some(idx);
         } else {
             return Err(EmeraldError::new(format!(
                 "Animation {} does not exist.",
@@ -119,13 +126,12 @@ impl Aseprite {
         Ok(())
     }
 
-    fn get_tag(&mut self, name: &str) -> Option<AsepriteTag> {
+    fn find_tag(&self, name: &str) -> Option<usize> {
         self.data
             .meta
             .frame_tags
             .iter()
-            .find(|tag| tag.name == name)
-            .cloned()
+            .position(|tag| tag.name == name)
     }
 
     fn reset(&mut self) {
@@ -140,22 +146,32 @@ impl Aseprite {
         let frame = self.get_frame();
         let duration = frame.duration as f32 / 1000.0;
 
+        let num_frames_in_tag: u32 = self
+            .get_current_tag()
+            .map(|tag| tag.to - tag.from)
+            .unwrap_or_default();
+
         while self.elapsed_time >= duration {
             self.elapsed_time -= duration;
             self.frame_counter += 1;
 
-            if self.frame_counter as u32 > (self.current_tag.to - self.current_tag.from) {
+            if self.frame_counter as u32 > num_frames_in_tag {
                 if self.is_looping {
                     self.frame_counter = 0;
                 } else {
-                    self.frame_counter = (self.current_tag.to - self.current_tag.from) as usize;
+                    self.frame_counter = num_frames_in_tag as usize;
                 }
             }
         }
     }
 
     fn get_frame(&self) -> &AsepriteFrame {
-        &self.data.frames[self.current_tag.from as usize + self.frame_counter]
+        let cur_tag_start: u32 = self
+            .get_current_tag()
+            .map(|tag| tag.from)
+            .unwrap_or_default();
+
+        &self.data.frames[cur_tag_start as usize + self.frame_counter]
     }
 }
 
