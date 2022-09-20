1efb65866172175746d42c56165f42dd302923e2
Agnis Aldiņš "NeZvērs <agnis16@gmail.com>
2022-09-21 02:22:24 +0300

    Sprites example - spawn & move (#190)

--- examples/sprites.rs
+++ examples/sprites.rs
@@ -19,7 +19,35 @@ impl Game for SpritesExample {
         self.world.spawn((sprite, Transform::default()));
     }
 
-    fn update(&mut self, _emd: Emerald) {}
+    fn update(&mut self, mut emd: Emerald) {
+        let mouse = emd.input().mouse();
+        let screen_size = emd.screen_size();
+        let mouse_position = screen_translation_to_world_translation(
+            (screen_size.0 as u32, screen_size.1 as u32),
+            &mouse.translation,
+            &mut self.world,
+        );
+        
+        // Spawn with left mouse
+        if mouse.left.is_just_pressed() {
+            let mut sprite = emd.loader().sprite("bunny.png").unwrap();
+            sprite.offset = Vector2::new(-10.0, 0.0);
+
+            let mut transform = Transform::default();
+            transform.translation = mouse_position;
+            self.world.spawn((sprite, transform));
+        }
+
+        // move to mouse position
+        if mouse.right.is_pressed {
+            let speed = 0.1 * emd.delta();
+
+            for (_, (transform, _sprite)) in self.world.query::<(&mut Transform, &mut Sprite)>().iter() {
+                transform.translation.x += (mouse_position.x - transform.translation.x) * speed;
+                transform.translation.y += (mouse_position.y - transform.translation.y) * speed;
+            }
+        }
+    }
 
     fn draw(&mut self, mut emd: Emerald) {
         emd.graphics().begin().unwrap();
