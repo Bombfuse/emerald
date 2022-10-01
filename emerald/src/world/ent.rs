16d679e64c95c9a884c08442a6de3fe1a54487a7
Bombfuse <eiffeldud@gmail.com>
2022-10-01 10:17:18 -0700

    remove ent

--- emerald/src/world/ent.rs
+++
@@ -1,3 +0,0 @@
-// .ent is a file that can be directly loaded into a World as an Entity
-// they should support custom components
-pub mod schemas;
--- emerald/src/world/ent/schemas.rs
+++
@@ -1,9 +0,0 @@
-use serde::{Deserialize, Serialize};
-
-pub mod sprite_schema;
-
-#[derive(Serialize, Deserialize)]
-pub struct Vector2Schema {
-    pub x: f32,
-    pub y: f32,
-}
