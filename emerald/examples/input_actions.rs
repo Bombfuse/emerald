97916931b922419569537c09e303a0d2156a9046
Clay Paris <clay@pop-os.localdomain>
2022-10-08 13:43:11 -0700

    Make button actions bind to a specific gamepad index

--- emerald/examples/input_actions.rs
+++ emerald/examples/input_actions.rs
@@ -32,11 +32,29 @@ impl Game for InputActionsExample {
             .add_action_binding_key(&ACTION_TEST.to_string(), KeyCode::P);
         emd.input()
             .add_action_binding_key(&ACTION_TEST.to_string(), KeyCode::G);
+
+        emd.input()
+            .add_action_binding_button(&ACTION_ATTACK_P1.to_string(), Button::West, 0);
+
+        emd.input()
+            .add_action_binding_button(&ACTION_ATTACK_P2.to_string(), Button::West, 1);
     }
 
     fn update(&mut self, mut emd: Emerald) {
         if emd.input().is_action_just_pressed(&ACTION_TEST.to_string()) {
             println!("Test Action Pressed");
+        }
+        if emd
+            .input()
+            .is_action_just_pressed(&ACTION_ATTACK_P1.to_string())
+        {
+            println!("p1 attack");
+        }
+        if emd
+            .input()
+            .is_action_just_pressed(&ACTION_ATTACK_P2.to_string())
+        {
+            println!("p2 attack");
         }
     }
 
