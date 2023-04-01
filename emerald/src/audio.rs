94fd4e8ce4889cf0d0e04ecb660df462490e615c
Clay Paris <clay@pop-os.localdomain>
2023-03-31 18:58:59 -0700

    wip

--- emerald/src/audio.rs
+++ emerald/src/audio.rs
@@ -1,3 +1,4 @@
+mod components;
 mod engine;
 mod handler;
 mod mixer;
---
+++ emerald/src/audio/components/sound_player.rs
@@ -0,0 +1,30 @@
+use std::{cell::Ref, collections::HashMap};
+
+use crate::{Emerald, EmeraldError, SoundInstanceId, SoundKey};
+
+/// Holds sound keys and can play them given Emerald.
+/// Mostly useful for preloading the sounds needed by Entities, or the World.
+pub struct SoundPlayer {
+    keys: HashMap<String, SoundKey>,
+    mixer: String,
+}
+impl SoundPlayer {
+    pub fn new<T: Into<String>>(mixer: T) -> Self {
+        Self {
+            mixer: mixer.into(),
+            keys: HashMap::new(),
+        }
+    }
+
+    pub fn play(&self, emd: &mut Emerald, label: &str) -> Result<SoundInstanceId, EmeraldError> {
+        if let Some(key) = self.keys.get(label) {
+            let id = emd.audio().mixer(&self.mixer)?.play(key)?;
+            return Ok(id);
+        }
+
+        Err(EmeraldError::new(format!(
+            "{:?} is not loaded into the Sound Player",
+            label
+        )))
+    }
+}
